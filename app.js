4bfece3861dc820b39b755df1d1d5c9f8989b3f8
gogiabu <146556307+gogiabu@users.noreply.github.com>
2024-05-14 11:15:08 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function moqnedebebi (x,y) {
+    document.write ( x+y + ("<br>"));
+    document.write ( x-y + ("<br>"));
+    document.write ( x*y + ("<br>"));
+    document.write ( x**y + ("<br>"));
+}
+moqnedebebi(5,2);
